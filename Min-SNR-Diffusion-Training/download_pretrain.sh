1702bf1888a577b21931b3e507943a15676f30af
Zhejun Jiang <jzj@gra-login2.graham.sharcnet>
2024-06-12 01:18:43 -0400

    updated 256*256 for snr

--- Min-SNR-Diffusion-Training/download_pretrain.sh
+++ Min-SNR-Diffusion-Training/download_pretrain.sh
@@ -24,3 +24,14 @@ else
     mkdir -p exp/guided_diffusion/;
     wget https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/releases/download/v0.0.0/ema_0.9999_large.pt -O $CKPT;
 fi
+
+
+CKPT="exp/guided_diffusion/ema_0.9999_xl.pt"
+
+if [ -e $CKPT ]; then
+    echo "$CKPT exists."
+else
+    echo "$$CKPT does not exist.";
+    mkdir -p exp/guided_diffusion/;
+    wget https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/releases/download/v0.0.0/ema_0.9999_xl.pt -O $CKPT;
+fi
